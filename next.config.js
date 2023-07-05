a25a84b84eb74a987c77561b43384420882832a4
Aliou Dione <alioudione@MBP-de-Aliou.lan>
2023-07-06 01:11:27 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -8,7 +8,10 @@ const nextConfig = {
         'lh3.googleusercontent.com',
         'res.cloudinary.com',
       ]
-    }
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+  },
   }
   
   module.exports = nextConfig
